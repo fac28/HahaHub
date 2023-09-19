dafc81327223e5e003b6695e9f2213d75576631e
JamesESS <60142087+JamesESS@users.noreply.github.com>
2023-09-19 16:14:37 +0100

    made post method logic
    Co-authored-by: Yuqing Wang <yuqingwwang@users.noreply.github.com>

--- test/sanitise.test.js
+++ test/sanitise.test.js
@@ -5,13 +5,13 @@ const { request } = require("./helpers.js");
 test("POST with script tag is correctly sanitized", async () => {
   const { status, body } = await request("/", {
     method: "POST",
-    body: "nickname=oli&message=<script>alert('uh oh')</script>",
+    body: "nickname=oli&jokeInput=<script>alert('uh oh')</script>",
     headers: { "content-type": "application/x-www-form-urlencoded" },
   });
   assert.equal(status, 200);
   assert.match(
     body,
-    /&lt;script>alert\('uh oh'\)&lt;\/script>/i,
+    /&lt;script&gt;alert\(&#39;uh oh&#39;\)&lt;\/script&gt;/i,
     `Expected <script> to have '<' replaced with '&lt;', but received:\n${body}`
   );
 });
