f7ad7398b9c3cc2182d68dbff5940e63cec9daed
Nicholas Galzin <dropout@Nicholass-MacBook-Air.local>
2023-09-20 11:37:21 +0100

    Delete route and id creation
    Closes #29

--- scripts/server.js
+++ scripts/server.js
@@ -12,10 +12,13 @@ app.use(staticHandler);
 const jokes = [
   {
     delivery: "A person walks into a bar",
-    nickname: "Bobby" },
+    nickname: "Bobby",
+    id: Math.random(),
+  },
   {
     delivery: "What do you get when you cross a elephant with a rhino?",
     nickname: "Samantha",
+    id: Math.random(),
   },
 ];
 
@@ -26,26 +29,43 @@ app.get("/", (req, res) => {
   res.render("index", { jokes: jokes });
 });
 
-app.post("/", (req,res) => {
+app.post("/", (req, res) => {
   let name = req.body.nickname;
   const joke = req.body.jokeInput;
+  let id = Math.random();
 
   // if name is empty, change to anonymous
-  if (name==="") {
-    name = 'anonymous'
+  if (name === "") {
+    name = "anonymous";
   }
 
   // if joke is empty, don't add to jokes, send 400
-  if (joke!== ""){
+  if (joke !== "") {
     jokes.push({
       delivery: joke,
-      nickname: name
+      nickname: name,
+      id: id,
     });
-    res.redirect("/");
-  }
-  else {
+    res.render("index", { jokes: jokes });
+  } else {
     res.status(400).send();
   }
+});
+
+//Delete route for posts
+app.post("/delete:id", (req, res) => {
+  const id = req.params.id;
+
+  // check if index is -1
+  let index = jokes.findIndex((joke) => {
+    return joke.id == id;
+  });
+
+  if (index!=-1){
+    jokes.splice(index, 1);
+  }
+
+  res.render("index", { jokes: jokes });
 });
 
 module.exports = app;
